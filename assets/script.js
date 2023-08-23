29681157d48482e1a4ecf33227b797441b804de0
kevinfreitasv80 <kevinfreitasv80@gmail.com>
2023-08-23 13:00:02 -0300

    E-commerce Product Page

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const articleX = document.querySelector(".articleX");
-const imgMain = document.querySelector(".imgMain");
+const imgMain = document.querySelectorAll(".imgMain");
 const aside = document.querySelector("aside");
+const productSelect = document.querySelectorAll(".imgsProduct>section:last-child article");
+const imgsProductSelect = document.querySelectorAll(".imgsProduct>section:last-child article img");
 const quantP = document.getElementById("quantP");
 const quantCartSpan = document.getElementById("quantCartSpan");
 let quantProduct = 0;
 
 const quant = (...val) => val.forEach(element => element.textContent = quantProduct);
+const replaceImgMain = (index, imgNum) => imgMain[index].setAttribute("src", `./images/image-product-${imgNum}.jpg`);
 
 plus.addEventListener("click", () => {
     quantProduct++;
@@ -26,6 +29,52 @@ window.addEventListener("resize", () => {
 articleX.addEventListener("click", () => {
     aside.style.display = "none";
 });
-imgMain.addEventListener("click", () => {
+imgMain[0].addEventListener("click", () => {
     aside.style.display = "block";
 });
+for (let i = 0; i < productSelect.length; i++) {
+    productSelect[i].addEventListener("click", function(){
+        for (let j = 0; j < productSelect.length; j++) {
+            imgsProductSelect[j].classList[0] === "selected" ? imgsProductSelect[j].classList.remove("selected") : undefined;
+        }
+        if (i < 4) {
+            switch (i) {
+                case 0:
+                    replaceImgMain(0, 1);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+                case 1:
+                    replaceImgMain(0, 2);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+                case 2:
+                    replaceImgMain(0, 3);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+                case 3:
+                    replaceImgMain(0, 4);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+            }
+        }else{
+            switch (i) {
+                case 4:
+                    replaceImgMain(1, 1);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+                case 5:
+                    replaceImgMain(1, 2);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+                case 6:
+                    replaceImgMain(1, 3);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+                case 7:
+                    replaceImgMain(1, 4);
+                    imgsProductSelect[i].classList.add("selected");
+                    break;
+            }
+        }
+    });
+}
