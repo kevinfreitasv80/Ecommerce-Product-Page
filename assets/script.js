e1ff72a5e795ee7c10a28169d16d2c9745a14cec
kevinfreitasv80 <kevinfreitasv80@gmail.com>
2023-08-26 11:55:59 -0300

    E-commerce Product Page

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,36 @@
+let oc = false;
+let quantProduct = 0;
+const priceProduct = 125;
 const plus = document.querySelector(".plus");
+const aside = document.querySelector("aside");
 const minus = document.querySelector(".minus");
+const quantP = document.getElementById("quantP");
+const emptyP = document.querySelector(".emptyP");
+const addCart = document.getElementById("addCart");
+const priceTotal = document.querySelector("strong");
 const articleX = document.querySelector(".articleX");
 const imgMain = document.querySelectorAll(".imgMain");
+const ballonCart = document.querySelector(".ballonCart");
+const iconCart = document.querySelector(".quantCart>.cart");
+const quantCartSpan = document.getElementById("quantCartSpan");
 const quantBallonCart = document.querySelector(".quantBallonCart");
-const addCart = document.getElementById("addCart");
-const aside = document.querySelector("aside");
-const priceTotal = document.querySelector("strong");
+const ballonCartSectionAndButton = document.querySelectorAll(".ballonCart section, .ballonCart button");
 const productSelect = document.querySelectorAll(".imgsProduct>section:last-child article");
 const imgsProductSelect = document.querySelectorAll(".imgsProduct>section:last-child article img");
-const quantP = document.getElementById("quantP");
-const quantCartSpan = document.getElementById("quantCartSpan");
-const priceProduct = 125;
-let quantProduct = 0;
 
 const quant = (...val) => val.forEach(element => element.textContent = quantProduct);
 const replaceImgMain = (index, imgNum) => imgMain[index].setAttribute("src", `./images/image-product-${imgNum}.jpg`);
 
 plus.addEventListener("click", () => {
     quantProduct++;
-    quant(quantP, quantCartSpan);
+    quant(quantP);
 });
 minus.addEventListener("click", () => {
     if (quantProduct < 1) {
         priceTotal.textContent = 0;
     }else{
         quantProduct--;
-        quant(quantCartSpan, quantP);
+        quant(quantP);
     }
 });
 window.addEventListener("resize", () => {
@@ -39,9 +44,26 @@ imgMain[0].addEventListener("click", () => {
 });
 addCart.addEventListener("click", () => {
     if (quantProduct > 0) {
-        quant(quantBallonCart);
-        priceTotal.textContent = parseFloat(priceProduct * quantProduct);
+        quantCartSpan.style.display = "block";
+        emptyP.style.display = "none";
+        for (let i = 0; i < ballonCartSectionAndButton.length; i++) {
+            switch (i) {
+                case 0:
+                    ballonCartSectionAndButton[i].style.display = "flex";
+                    break;
+                case 1:
+                    debugger
+                    ballonCartSectionAndButton[i].style.display = "block";
+                    break;
+            }
+        }
+        quant(quantBallonCart, quantCartSpan);
+        priceTotal.textContent = (priceProduct * quantProduct).toFixed(2);
     }
+});
+iconCart.addEventListener("click", () => {
+    !oc ? ballonCart.style.display = "block" : ballonCart.style.display = "none";
+    oc = !oc;
 });
 for (let i = 0; i < productSelect.length; i++) {
     productSelect[i].addEventListener("click", function(){
